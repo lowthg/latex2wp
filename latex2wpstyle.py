1417e168e376acf6274b2d0d973f785906346d1f
George Lowther <37965159+lowthg@users.noreply.github.com>
2023-01-04 23:30:19 +0000

    fix td style

--- latex2wpstyle.py
+++ latex2wpstyle.py
@@ -79,7 +79,7 @@ subsectionstar = "\n<p>\n<hr>\n<h4_SecLabel_> _SecName_ </h4>\n<p>\n"
 
 # styling for math table
 eqtblstyle = "style=\"width:100%;border:none;margin-top:0;margin-bottom:0\""
-eqtdstyle = "style=\"width:100%;border:none;text-align:center;padding:0\""
+eqtdstyle = "style=\"border:none;text-align:center;padding:0\""
 
 # Font styles. Feel free to add others. The key *must* contain
 # an open curly bracket. The value is the namem of a HTML tag.
